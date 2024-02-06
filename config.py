f12e0a118488a549f45cab1ddf20b0b166a5ccb2
wiktor bozek <wiktorsebbozek@gmail.com>
2024-02-06 21:08:17 +0100

    Create config file

---
+++ config.py
@@ -0,0 +1,24 @@
+# Plik konfiguracyjny
+
+activation_word="spotify"
+
+play_prompts=['wnów', 'odtwórz', 'włącz', 'play']
+
+stop_prompts=['stop', 'zatrzymaj', 'wyłącz']
+
+skip_prompts=['pomiń', 'przewiń', 'skip', 'dalej', 'następna', 'następny', 'następną']
+
+volume_prompts=['ustaw głośność', 'zmień głośność']
+
+volume_down_prompts=['zmniejsz głośność', 'ścisz']
+
+volume_up_prompts=['zwiększ głośność', 'podgłośnij']
+
+current_track_prompts=['jak się nazywa', 'jaka to piosenka', 'podaj nazwę', 'co teraz leci']
+
+
+
+
+
+
+
